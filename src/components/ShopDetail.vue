83773a9798d0df10e17bda243cfca5a11c30aafd
松井英俊 <partyrockeverydayimshufflin@gmail.com>
2020-04-06 21:10:30 +0900

    店舗詳細追加

--- src/components/ShopDetail.vue
+++ src/components/ShopDetail.vue
@@ -10,23 +10,35 @@
                         v-bind:key="index"
                         xs6 sm6 md4 text-center class="pa-3"
                         >
-                        <v-card min-height="400px" class="pa-3">
-                            <h4>{{ item.name }}</h4>
-                            <img class="item_image" v-bind:src="require('@/assets/shops/' + shop.key + '/items/' + item.image_path)" alt="">
-                            <p>{{ item.description }}</p>
-                            <p>¥{{ item.price.toLocaleString() }}-</p>
-                        </v-card>
+                            <v-card min-height="400px" class="pa-3">
+                                <h4>{{ item.name }}</h4>
+                                <img class="image" v-bind:src="require('@/assets/shops/' + shop.key + '/items/' + item.image_path)" alt="">
+                                <p>{{ item.description }}</p>
+                                <p>¥{{ item.price.toLocaleString() }}-</p>
+                            </v-card>
                         </v-flex>
                     </v-row>
                 </v-col>
+                <v-col cols="12">
+                    <v-card>
+                        <h4 class="pa-3">{{ shop.name }}</h4>
+                        <img class="image pa-3" v-bind:src="require('@/assets/shops/' + shop.key + '/main.jpg')" alt="">
+                        <p class="pa-3">{{ shop.description }}</p>
+                    </v-card>
+                </v-col>
             </v-row>
         </v-container>
+        <Footer v-bind:shop="shop" />
     </div>
 </template>
 <script>
+import Footer from './Footer'
 import shops from '../data/shops.json'
 export default {
     name: "Shop",
+    components: {
+        Footer
+    },
     data() {
         return {
             shop: shops[this.$route.params.key]
@@ -35,7 +47,10 @@ export default {
 }
 </script>
 <style>
-.item_image {
+.image {
     width: 100%;
+}
+.shop {
+    margin-bottom: 50px;
 }
 </style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,7 +8,7 @@ export default new Vuetify({
     themes: {
       light: {
         primary: '#eb3f25',
-        secondary: '#424242',
+        secondary: '#ffd992',
         accent: '#82B1FF',
         error: '#FF5252',
         info: '#2196F3',
